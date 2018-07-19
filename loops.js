e7ea5a3663719274abe9e3a42c32fde838e9cb59
bnreinecke0209@gmail.com <bnreinecke0209@gmail.com>
2018-07-19 19:29:54 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -8,26 +8,24 @@ function forLoop(array) {
     return array;
 }
 
-
+function whileLoop(n) {
+  while(n>=0) {
+    
+  }
+  
+}
 
 
 /*
-`"I am ${i} strange loops."`.
+  describe('whileLoop(n)', () => {
+    it('counts down from n to 0', () => {
+      const spy = chai.spy.on(console, 'log')
+      const n = Math.floor(Math.random() * 100)
 
+      expect(whileLoop(n)).to.equal('done')
+      expect(spy).to.have.been.called.exactly(n)
 
-  describe('forLoop(array)', () => {
-    it('adds `"I am ${i} strange loop${i === 0 ? \'\' : \'s\'}."` to an array 25 times', () => {
-      const [array, t] = makeArray()
-      const strangeArray = forLoop(array)
-
-      expect(strangeArray.length).to.equal(t + 25)
-
-      const testArray = strangeArray.slice(array.length)
-
-      for (let i = 0, l = testArray.length; i < l; i++) {
-        let s = i === 1 ? "I am 1 strange loop." : `I am ${i} strange loops.`
-        expect(testArray[i]).to.equal(s)
-      }
+      console.log.reset()
     })
   })
 */
