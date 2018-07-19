8d0cc251a13eb40c83f1544856e0ba75d211ed4b
bnreinecke0209@gmail.com <bnreinecke0209@gmail.com>
2018-07-19 19:32:37 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -17,16 +17,8 @@ function whileLoop(n) {
 }
 
 
+
+
 /*
-  describe('whileLoop(n)', () => {
-    it('counts down from n to 0', () => {
-      const spy = chai.spy.on(console, 'log')
-      const n = Math.floor(Math.random() * 100)
 
-      expect(whileLoop(n)).to.equal('done')
-      expect(spy).to.have.been.called.exactly(n)
-
-      console.log.reset()
-    })
-  })
 */
